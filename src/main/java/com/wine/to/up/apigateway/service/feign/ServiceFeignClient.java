51a09424f48cb327c631ad72acafa3fa62273f68
santatostada <asabinayulia@gmail.com>
2020-10-29 18:59:14 +0300

    adding request to user-service

--- src/main/java/com/wine/to/up/apigateway/service/feign/ServiceFeignClient.java
+++ src/main/java/com/wine/to/up/apigateway/service/feign/ServiceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "http://localhost:8080")
+@FeignClient(name = "user-service")
 public interface ServiceFeignClient {
 
     @PostMapping("/validate")
