5ccae4f58ce34f32067bbf92f38e96391c441706
santatostada <asabinayulia@gmail.com>
2020-11-01 19:04:37 +0300

    adding user service url to feign client

--- src/main/java/com/wine/to/up/apigateway/service/feign/ServiceFeignClient.java
+++ src/main/java/com/wine/to/up/apigateway/service/feign/ServiceFeignClient.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "http://user-service:8080")
 public interface ServiceFeignClient {
 
     @PostMapping("/validate")
     ResponseEntity<Void> validate(@RequestParam String token);
-
-
 }
