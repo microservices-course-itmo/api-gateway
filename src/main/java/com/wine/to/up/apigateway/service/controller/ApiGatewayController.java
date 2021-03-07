5ccee6dba494f62bd58deb643a8ddd249457618e
santatostada <asabinayulia@gmail.com>
2021-03-07 19:29:21 +0300

    update wine position controller

--- src/main/java/com/wine/to/up/apigateway/service/controller/ApiGatewayController.java
+++ src/main/java/com/wine/to/up/apigateway/service/controller/ApiGatewayController.java
@@ -1,16 +1,17 @@
 package com.wine.to.up.apigateway.service.controller;
 
 import com.netflix.zuul.context.RequestContext;
+import com.wine.to.up.apigateway.service.dto.WinePositionWithFavorites;
 import com.wine.to.up.apigateway.service.jwt.JwtTokenProvider;
+import com.wine.to.up.apigateway.service.service.FavoritePositionService;
 import com.wine.to.up.catalog.service.api.dto.WinePositionTrueResponse;
 import com.wine.to.up.catalog.service.api.feign.FavoriteWinePositionsClient;
-import com.wine.to.up.catalog.service.api.service.FavoriteWinePositionsService;
+import com.wine.to.up.catalog.service.api.feign.WinePositionClient;
 import com.wine.to.up.user.service.api.dto.ItemDto;
 import com.wine.to.up.user.service.api.feign.FavoritesServiceClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +36,10 @@ public class ApiGatewayController {
 
     private final FavoriteWinePositionsClient favoriteWinePositionsClient;
 
+    private final WinePositionClient winePositionClient;
+
+    private final FavoritePositionService favoritePositionService;
+
 
     @ApiOperation(value = "Get favourites wine positions",
             nickname = "getFavouritesPositions",
@@ -52,6 +55,7 @@ public class ApiGatewayController {
 
         List<ItemDto> itemDtos = favoritesServiceClient.findUsersFavorites(id, role);
         List<String> ids = itemDtos.stream().map(ItemDto::getId).collect(Collectors.toList());
+
         return favoriteWinePositionsClient.getFavourites(ids);
     }
 
@@ -73,8 +77,7 @@ public class ApiGatewayController {
         List<ItemDto> itemDtos = favoritesServiceClient.findUsersFavorites(id, role);
         List<String> ids = itemDtos.stream().map(ItemDto::getId).collect(Collectors.toList());
         List<WinePositionTrueResponse> positions = winePositionClient.getAllWinePositionsTrue(page, amount, sortByPair, filterBy);
-        List<WinePositionWithFavorites> newPositions = favoritePositionService.convertWinePositions(positions, ids);
-        return favoritePositionService.getFavorites(newPositions);
+        return favoritePositionService.convertWinePositions(positions, ids);
     }
 
 }
