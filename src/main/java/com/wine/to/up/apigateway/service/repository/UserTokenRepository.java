b99109ea4a29bff0756ef40bb622ac12968d15da
santatostada <54457208+santatostada@users.noreply.github.com>
2021-03-22 23:00:11 +0300

    Revert "Favorites"

--- src/main/java/com/wine/to/up/apigateway/service/repository/UserTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wine.to.up.apigateway.service.repository;
-
-public interface UserTokenRepository {
-    boolean containsToken(String token);
-
-    void addToken(String token);
-
-    void clearToken(String token);
-}
